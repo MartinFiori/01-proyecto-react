914b76d1dd0341f37e0855b9d26f0d3f47d31689
Martín Fiori <martinfiori@outlook.com>
2022-01-26 01:12:39 -0300

    copy done

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,30 +4,15 @@ import { Link } from 'react-router-dom'
 
 const Item =({data})=>{
     const [liked, setLiked] = useState(false);
-    const { name, price, img, id, description } = data
-    // const { addFavorites, removeFavorite, favorites } = useContext(FavoriteContext)
-    // const [favorite] = useState(
-    //     {
-    //         id: data.id,
-    //         img: data.img,
-    //         price: data.price,
-    //         name: data.name,
-    //     }
-    // );
+    const [itemCopied, setItemCopied] = useState(false);
+    const { name, price, img, id } = data
 
-    // useEffect(() => {
-    //         localStorage.setItem('favorites', JSON.stringify(favorites))
-    // }, [liked]);
-    
-    // const sendFavorite = ()=>{
-    //     if (!liked) {
-    //         addFavorites(favorite)
-    //         console.log("el favorito es true: ", favorite);
-    //     } else{
-    //         removeFavorite(favorite)
-    //         console.log("el favorito es falso: ", favorite);
-    //     }
-    // }
+    const copied = () =>{
+        setItemCopied(true)
+        setTimeout(() => {
+            setItemCopied(false)
+        }, 2000);
+    }
     
     return(
         <div className="card">
@@ -36,16 +21,14 @@ const Item =({data})=>{
                 <p className="card__header--price">{`$${price}`}</p>
             </section>
             <img className="card__img" src={require(`../../assets/cards/${img}`)} alt={name} />
-            <p className="card__description">
-                {description}
-            </p>
             <section className="card__buttons">
                 <Link to={`/detail/${id}`}>
                     <button className="card__buttons--buy">Ver Producto</button>
                 </Link>
                 <div className="card__buttons--container">
                     <i className={`far fa-heart card__buttons--icon ${liked && "fas card__buttons--heart"}`} onClick={()=> setLiked(!liked)}></i>
-                    <i className="fas fa-share-alt card__buttons--icon" onClick={(e)=> {navigator.clipboard.writeText(e.target.parentElement.parentElement.firstChild.href)}}></i>
+                    <i className={`fas fa-share-alt card__buttons--icon`} onClick={(e)=> {navigator.clipboard.writeText(e.target.parentElement.parentElement.firstChild.href); copied()}}></i>
+                    <p className={`copied ${itemCopied ? "copied--active" : ""}`}>¡Copiado!</p>
                 </div>
             </section>
         </div>
