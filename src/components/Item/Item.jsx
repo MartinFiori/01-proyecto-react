cc233253fa224c7cf64e83446628ee5339f747ca
Martín Fiori <martinfiori@outlook.com>
2022-01-28 13:10:13 -0300

    adjusting item height values

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,20 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Item.css'
+
+// React Router Dom
 import { Link } from 'react-router-dom'
 
-const Item =({data})=>{
+const Item = ({ data }) => {
     const [liked, setLiked] = useState(false);
     const [itemCopied, setItemCopied] = useState(false);
     const { name, price, img, id } = data
 
-    const copied = () =>{
+    const copied = () => {
         setItemCopied(true)
         setTimeout(() => {
             setItemCopied(false)
         }, 2000);
     }
-    
-    return(
+
+    return (
         <div className="card">
             <section className="card__header">
                 <h3 className="card__header--title">{name}</h3>
@@ -26,8 +28,8 @@ const Item =({data})=>{
                     <button className="card__buttons--buy">Ver Producto</button>
                 </Link>
                 <div className="card__buttons--container">
-                    <i className={`far fa-heart card__buttons--icon ${liked && "fas card__buttons--heart"}`} onClick={()=> setLiked(!liked)}></i>
-                    <i className={`fas fa-share-alt card__buttons--icon`} onClick={(e)=> {navigator.clipboard.writeText(e.target.parentElement.parentElement.firstChild.href); copied()}}></i>
+                    <i className={`far fa-heart card__buttons--icon ${liked && "fas card__buttons--heart"}`} onClick={() => setLiked(!liked)}></i>
+                    <i className={`fas fa-share-alt card__buttons--icon`} onClick={(e) => { navigator.clipboard.writeText(e.target.parentElement.parentElement.firstChild.href); copied() }}></i>
                     <p className={`copied ${itemCopied ? "copied--active" : ""}`}>¡Copiado!</p>
                 </div>
             </section>
