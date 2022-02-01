bde3bbb69167213b0888f4ef3e761af1e072777b
Martín Fiori <martinfiori@outlook.com>
2022-02-01 18:12:47 -0300

    changing item design

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -20,18 +20,17 @@ const Item = ({ data }) => {
         <div className="card">
             <section className="card__header">
                 <h3 className="card__header--title">{name}</h3>
-                <p className="card__header--price">{`$${price}`}</p>
+                <div className="card__buttons--container">
+                    <i className={`fas fa-share-alt card__buttons--icon`} onClick={(e) => { navigator.clipboard.writeText(e.target.parentElement.parentElement.firstChild.href); copied() }}></i>
+                    <p className={`copied ${itemCopied ? "copied--active" : ""}`}>¡Copiado!</p>
+                </div>
             </section>
             <img className="card__img" src={require(`../../assets/cards/${img}`)} alt={name} />
             <section className="card__buttons">
                 <Link to={`/detail/${id}`}>
                     <button className="card__buttons--buy">Ver Producto</button>
                 </Link>
-                <div className="card__buttons--container">
-                    <i className={`far fa-heart card__buttons--icon ${liked && "fas card__buttons--heart"}`} onClick={() => setLiked(!liked)}></i>
-                    <i className={`fas fa-share-alt card__buttons--icon`} onClick={(e) => { navigator.clipboard.writeText(e.target.parentElement.parentElement.firstChild.href); copied() }}></i>
-                    <p className={`copied ${itemCopied ? "copied--active" : ""}`}>¡Copiado!</p>
-                </div>
+                <p className="card__header--price">{`$${price}`}</p>
             </section>
         </div>
     )
