06ed592c1303e04a5e8b9a56331528a54090e097
MartinFiori <martinfiori@outlook.com>
2024-02-08 22:40:52 -0300

    Deleting adminpage

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -12,31 +12,29 @@ import Contact from "../../pages/ContactPage/ContactPage.jsx";
 import PaymentContainer from "../../pages/PaymetContainer/PaymentContainer.jsx";
 import TicketPage from "../../pages/TicketPage/TicketPage";
 import Footer from "../../components/Footer/Footer.jsx";
-import AdminPage from "../../pages/Admin/AdminPage";
 
 // Context
 import { CartProvider } from "../../context/CartContext/CartContext";
 
 const AppRouter = () => {
-	return (
-		<BrowserRouter>
-			<CartProvider>
-				<NavBar />
-				<Routes>
-					<Route path="/detail/:id" element={<ItemDetailContainer />} />
-					<Route path="/admin" element={<AdminPage />} />
-					<Route path="/cart" element={<CartPage />} />
-					<Route path="/about-us" element={<AboutUs />} />
-					<Route path="/contact-us" element={<Contact />} />
-					<Route path="/ticket-page" element={<TicketPage />} />
-					<Route path="/payment-process" element={<PaymentContainer />} />
-					<Route path="/" element={<ItemListContainer />} />
-					<Route path="*" element={<Error404Page />} />
-				</Routes>
-			</CartProvider>
-			<Footer />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/detail/:id" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/ticket-page" element={<TicketPage />} />
+          <Route path="/payment-process" element={<PaymentContainer />} />
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="*" element={<Error404Page />} />
+        </Routes>
+      </CartProvider>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
